abfb4f3306271a2ee7dcd509b3516626e2155ddc
Maksura Nuha <94732532+Nuha24@users.noreply.github.com>
2022-02-19 12:41:39 +0600

    Update 10038.cpp

--- 10038.cpp
+++ 10038.cpp
@@ -1,11 +1,27 @@
-//C-
-#include <stdio.h>
+//10038-uva
+#include<bits/stdc++.h>
+using namespace std;
 int main()
 {
-   long long int X,Y;
-   scanf("%lld %lld",&X,&Y);
-   printf("%lld + %lld = %lld\n",X,Y,(X+Y));
-   printf("%lld * %lld = %lld\n",X,Y,(X*Y));
-   printf("%lld - %lld = %lld\n",X,Y,(X-Y));
-   return 0;
+	int n, a[10000], i;
+	while(cin>>n)
+    {
+		for(i = 0; i < n; i++)
+		cin>>a[i];
+		int s[10000], flag = 0;
+		for(i = 1; i < n; i++)
+		{
+        if(abs(a[i] - a[i-1]) < n)
+        s[abs(a[i] - a[i-1])]++;
+		}
+		for(i = 1; i < n; i++)
+        if(s[i] == 0)
+        {
+        flag = 1;
+        }
+		if(flag==0)
+		cout<<"Jolly"<<endl;
+		else
+		cout<<"Not jolly"<<endl;
+	}
 }
